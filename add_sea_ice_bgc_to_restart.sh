5ec65b4b6edefde82d2bdb0f8774a12ed9bcde86
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2021-11-19 12:06:21 +1100

    add n_surf, alg_surf to ocean_sbc.res.nc in add_sea_ice_bgc_to_restart.sh

--- add_sea_ice_bgc_to_restart.sh
+++ add_sea_ice_bgc_to_restart.sh
@@ -8,13 +8,13 @@ set -e
 #Below, you need to define 'path2restart' and 'filename' for the restart file.
 
 #Define the path to the restart file you want to add BGC tracers
-path2restart=/g/data/ik11/restarts/access-om2-01/01deg_jra55v140_iaf_cycle3/restart731/ice
+path2restart=/g/data/ik11/restarts/access-om2-01/01deg_jra55v140_iaf_cycle3/restart731
 
 #Define the name of the restart file
 filename=iced.2019-01-01-00000.nc
 
 #Copy the restart file to pwd
-cp ${path2restart}/${filename} .
+cp ${path2restart}/ice/${filename} .
 
 #Add 2D fields.
 for j in algalN nit
@@ -33,7 +33,7 @@ done
 filename=i2o.nc
 
 #Copy the restart file to pwd
-cp ${path2restart}/${filename} .
+cp ${path2restart}/ice/${filename} .
 
 for j in wnd10_io nit_io alg_io
 do
@@ -43,9 +43,25 @@ done
 filename=o2i.nc
 
 #Copy the restart file to pwd
-cp ${path2restart}/${filename} .
+cp ${path2restart}/ice/${filename} .
 
 for j in ssn_i ssalg_i
 do
     ncap2 -O -s ${j}=sst_i*0 ${filename} ${filename}
+done
+
+# Also ocean_sbc.res.nc
+
+filename=ocean_sbc.res.nc
+
+#Copy the restart file to pwd
+cp ${path2restart}/ocean/${filename} .
+
+for j in n_surf alg_surf
+do
+    ncap2 -O -s ${j}=t_surf*0 ${filename} ${filename}
+    for a in long_name units checksum
+    do # remove attributes
+        ncatted -O -a ${a},${j},d,, ${filename} ${filename}
+    done
 done
